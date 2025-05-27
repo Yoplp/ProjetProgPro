2a0d21aa4cf6f6809fa65a5c3b8a5d5e2bf44eb7
wang <kw779966@gmail.com>
2025-05-27 14:04:19 +0200

    Add isRunning attr to gameState

--- src/game/GameState.java
+++ src/game/GameState.java
@@ -4,18 +4,19 @@ import map.GameMap;
 import character.Player;
 
 public class GameState {
-    private static GameState instance; 
-    
+    private static GameState instance;
     private GameMap map;
     private Player joueur;
+    private boolean isRunning;
 
     private GameState() { 
         this.map = new GameMap();
-        this.joueur = new Player("Salvateur Stellaire dis SS", 10, map.getStart()); 
+        this.joueur = new Player("Salvateur Stellaire dis SS", 10, map.getStartRoom());
+        this.isRunning = true;
     }
 
     public static GameState getInstance() {
-        if (instance == null) {
+            if (instance == null) {
             instance = new GameState();
         }
         return instance;
@@ -23,5 +24,17 @@ public class GameState {
 
     public Player getJoueur() {
         return joueur;
+    }
+
+    public GameMap getMap() {
+        return map;
+    }
+
+    public void start() {
+        isRunning = true;
+    }
+
+    public void stop() {
+        isRunning = false;
     }
 }
